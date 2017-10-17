d2f13a9d5ddc00d9209fdfb494e6bbc84026bb74
jinredhub <jin@nieblo.com>
2017-10-16 18:02:35 -0700

    fixtd data array

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,8 @@
 
 var surveyArray = [{
   "name":"Obama",
-  "photo":"http://img.timeinc.net/time/photoessays/2008/people_who_mattered/obama_main_1216.jpg":[
+  "photo":"http://img.timeinc.net/time/photoessays/2008/people_who_mattered/obama_main_1216.jpg",
+  "scores":[
       5,
       1,
       4,
